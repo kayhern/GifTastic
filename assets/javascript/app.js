5c709c550cea021cf08da1fddedabcc301f1509c
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-26 10:00:14 -0400

    clearing gif images and rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,20 +26,23 @@ function addButtons() {
 addButtons();
 
 function clearGifs() {
-    //if another button is clicked then the previous buttons should clear
-}
+    //if another button is clicked then the previous buttons should clear source: https://api.jquery.com/remove/
+    $("button").click(function() {
+        $("img").remove();
+        $("span").remove();
+      });
+};
 
 //if the gif is clicked it should animate
 
 $("button").on("click", function(){
  //defining the button being clicked at the top
  let gifSubject = $(this).attr("random-shtuff");
-
  //defining the URL I am querying using my own API key, the button referral from the HTML, ten results limit
  //"random-shtuff" is what I am calling my buttons because I did not have a specific theme
  let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
  gifSubject + "&api_key=YvIPVJfdVoqpfFqTghWIjsoMK19EoSfc&limit=10";
-
+ clearGifs();
  //the AJAX stuff should go here - get method to get the gifs
  $.ajax({
     url: queryURL,
@@ -48,12 +51,12 @@ $("button").on("click", function(){
  //then function after GET
 .then(function(response) {
     let results = response.data;
-//borrowing for loop from class activity
+//borrowing function from class activity: https://harvard.bootcampcontent.com/Harvard-Coding-Boot-Camp/hu-cam-fsf-pt-09-2019-u-c/blob/master/Week_6/01-Activities/14-DynamicElements/Solved/dynamic-elements-solution.html
     for (let i = 0; i < results.length; i++) {
         if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
             let gifDiv = $("<div>");
             let rating = results[i].rating;
-            let p = $("<div>").text("Rating " +  rating);
+            let p = $("<span>").text("Rating " +  rating);
             let gifImage = $("<img>");
             gifImage.attr("src", results[i].images.fixed_height.url);
             gifDiv.append(p);
@@ -63,12 +66,8 @@ $("button").on("click", function(){
         }
     }
 })
-})
-
-//THEN when another button is clicked it clears away the previous query
-
+});
 //PART TWO:
 //make the search bar work
-
 //PART THREE:
 //when gif is clicked it animates
